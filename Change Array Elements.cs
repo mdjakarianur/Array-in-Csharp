7b49b826df17666eedbe4457e9a163bf47485c5b
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-30 00:08:26 +0600

    Create Change Array Elements.cs

---
+++ Change Array Elements.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ChangeArray {
+  class Program {
+    static void Main(string[] args) {
+
+      // create an array
+      int[] numbers = {1, 2, 3};
+
+      Console.WriteLine("Old Value at index 0: " + numbers[0]);
+
+      // change the value at index 0
+      numbers[0] = 11;
+
+      //print new value
+      Console.WriteLine("New Value at index 0: " + numbers[0]);
+
+      Console.ReadLine();
+    }
+  }
+}
