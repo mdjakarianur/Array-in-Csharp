cb37f933396ebf15e454fe753f64ac948dcbcf04
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-30 00:07:40 +0600

    Create Basic Array Code.cs

---
+++ Basic Array Code.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace AccessArray {
+  class Program  {
+    static void Main(string[] args) {
+
+      int[] numbers = {1, 2, 3};
+
+      Console.WriteLine("Element in first index : " + numbers[0]);
+
+      Console.WriteLine("Element in second index : " + numbers[1]);
+
+      Console.WriteLine("Element in third index : " + numbers[2]);
+
+      Console.ReadLine();
+
+    }
+  }
+}
