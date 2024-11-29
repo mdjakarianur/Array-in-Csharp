1a39795d5628ca3e340b13d218163da19a907d8f
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-30 00:10:02 +0600

    Create Find Minimum and Maximum Element.cs

---
+++ Find Minimum and Maximum Element.cs
@@ -0,0 +1,21 @@
+using System;
+
+ // provides us various methods to use in an array
+using System.Linq;
+
+namespace ArrayMinMax {
+  class Program  {
+    static void Main(string[] args) {
+
+      int[] numbers = {51, 1, 3, 4, 98};
+
+      // get the minimum element
+      Console.WriteLine("Smallest  Element: " + numbers.Min());  
+
+      // Max() returns the largest number in array
+      Console.WriteLine("Largest Element: " + numbers.Max());  
+ 	 
+      Console.ReadLine();
+    }
+  }
+}
