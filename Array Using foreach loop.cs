86bedf1b0e3c2539d835695a71de6c3ff6f697b0
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-30 00:09:29 +0600

    Create Array Using foreach loop.cs

---
+++ Array Using foreach loop.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AccessArrayForeach {
+  class Program {
+    static void Main(string[] args) {
+      int[] numbers = {1, 2, 3};
+
+      Console.WriteLine("Array Elements: ");
+
+      foreach(int num in numbers) {
+        Console.WriteLine(num);
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
